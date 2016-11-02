be8b5c835a41ec2f4e561588392f3df1805f4771
chaelcodes <rachaellw5.1@gmail.com>
2016-11-02 16:19:44 +0000

    Removed uneccesary code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-    render html: "hey you. You can do this. I am text, on a page, that you put here. If you can do that, you can do anything."
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -45,5 +45,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'application#hello'
+  root 'boards_url'
 end
