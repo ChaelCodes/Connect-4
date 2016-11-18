eb485814e96c7c41ca53587254d3f2bd88094f5a
chaelcodes <rachaellw5.1@gmail.com>
2016-11-18 15:25:00 +0000

    Migrated to Postgres, because Heroku doesn't support sqllite3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101184524) do
+ActiveRecord::Schema.define(version: 20161101153311) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "boards", force: :cascade do |t|
     t.string   "board_state"
